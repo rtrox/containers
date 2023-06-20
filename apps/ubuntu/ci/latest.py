9504199b77cb6ad229c9b943152495e5bbe9012d
Russell Troxel <russelltroxel@gmail.com>
2023-06-19 18:39:59 -0700

    Add ubuntu base image
    Signed-off-by: Russell Troxel <russelltroxel@gmail.com>

---
+++ apps/ubuntu/ci/latest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import requests
+import json
+
+# Get the latest version of Alpine Linux
+
+URL = "https://registry.hub.docker.com/v2/repositories/library/ubuntu/tags?ordering=name&name="
+
+def get_latest(channel):
+    r = requests.get(URL + channel)
+    data = json.loads(r.text)
+    for tag in data['results']:
+        if channel in tag['name']:
+            return tag['name']
+
+if __name__ == "__main__":
+    import sys
+    channel = sys.argv[1]
+    print(get_latest(channel))
